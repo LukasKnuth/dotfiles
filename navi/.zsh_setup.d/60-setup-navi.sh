a90573d36245d21e059ff8d801fc4db52e9a98df
Lukas Knuth <me@lknuth.dev>
2024-06-17 18:22:57 +0200

    Navi: Fix ZSH widget with zsh-vi-mode.
    As far as I understand, the "zsh-vi-mode" plugin builds on top of the ZLE builtin VI mode and simple redefined most functions/widgets to work better/support more features. In doing so, it also sets the input mode to VI.
    
    This seems to cause an issue with the default Navi widget (https://github.com/denisidoro/navi/issues/328). Looking at the output of `bindkey` after sourcing the file, the function set for CTRL-G isn't overwritten by Navi. I assume this is because Navis `bindkey` command does not specify the keymap.
    
    To fix this, we explicitly define the keymap command again, this time specifically for the `viins` keymap. This works as expected then.

--- navi/.zsh_setup.d/60-setup-navi.sh
+++ navi/.zsh_setup.d/60-setup-navi.sh
@@ -5,3 +5,9 @@ export NAVI_PATH="~/.my_cheats:$(navi info cheats-path)"
 
 # Setup shell integration
 eval "$(navi widget zsh)"
+
+# The default "bindkey" command in the eval above doesn't set the keymap. I _think_
+# this is why this doesn't work if the zsh-vi-mode plugin is loaded. We specfically
+# set the CTRL-G keybind for the VI INSert mode.
+bindkey -M viins '^g' _navi_widget
+
