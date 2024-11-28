91b9f8e7cac38e5e409f82c8868f08e9070e9deb
Lukas Knuth <me@lknuth.dev>
2024-11-28 18:06:35 +0100

    Git: Hide remote branches when tap-completing "git switch"
    There are usually many remote branches (especially on mono-repos) that colleagues are working on. I just want the ones I have locally, because those are usually the ones I switch between to work on.

--- git/.zsh_setup.d/60-git-alias.sh
+++ git/.zsh_setup.d/60-git-alias.sh
@@ -1,2 +1,7 @@
 # Faster than typing it full all the time.
 alias g='git'
+
+# Do not include remote branches when tap-completing
+# "git switch" or "git checkout" commands
+# Taken from: https://stackoverflow.com/a/43747486/717341
+export GIT_COMPLETION_CHECKOUT_NO_GUESS=1
