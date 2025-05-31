76e977c2bd2dc5343d34fd8250c4189cd90bc7ee
Lukas Knuth <me@lknuth.dev>
2025-05-31 22:36:57 +0200

    Brew: FIX - ensure brew adds to `PATH` first.
    Otherwise, any scripts that want to run setup with tools that are installed via brew will not find the commands. Like `fzf` or `direnv` for example.

--- brew/.zsh_setup.d/01-homebrew.sh
+++ brew/.zsh_setup.d/01-homebrew.sh
@@ -1,3 +1,5 @@
+# Adds the Brew installed tools to `PATH`. Must run first.
+# 
 # This is the default for Apple Silicon.
 # If more flexible setup is required, see the oh-my-zsh plugin
 local BREW_LOCATION="/opt/homebrew/bin/brew"
