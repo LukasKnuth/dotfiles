4758d40494529af3c971b506b6256c0c32faca7e
Lukas Knuth <me@lknuth.dev>
2025-07-19 11:49:37 +0200

    Git: Migrate alias setup to Fish.

--- git/.config/fish/conf.d/60-git-alias.fish
+++ git/.config/fish/conf.d/60-git-alias.fish
@@ -1,7 +1,6 @@
-# Faster than typing it full all the time.
 alias g='git'
 
 # Do not include remote branches when tap-completing
 # "git switch" or "git checkout" commands
 # Taken from: https://stackoverflow.com/a/43747486/717341
-export GIT_COMPLETION_CHECKOUT_NO_GUESS=1
+set -gx GIT_COMPLETION_CHECKOUT_NO_GUESS 1
